a8318bd0ad55ca168f6bffdabf6b3d11d0b01c3c
fadhilmayati <79211591+fadhilmayati@users.noreply.github.com>
2023-02-26 18:48:34 +0800

    Create password_gen.py
    new code

---
+++ password_gen.py
@@ -0,0 +1,25 @@
+# Created by Fadhil Mayati
+# Date 26 February 2023
+
+import string
+import random
+
+def generate_password(length, special_chars):
+    # Define the character set to use for the password
+    chars = string.ascii_letters + string.digits
+    if special_chars:
+        chars += string.punctuation
+
+    # Generate the password
+    password = ''.join(random.choice(chars) for i in range(length))
+    return password
+
+# Get user input
+length = int(input("Enter the password length: "))
+special_chars = input("Include special characters? (y/n): ").lower() == 'y'
+
+# Generate the password
+password = generate_password(length, special_chars)
+
+# Print the password
+print("Your password is:", password)
